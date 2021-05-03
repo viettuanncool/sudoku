03f3abda383de0d118a199442a3e0a59b8f6443a
nguyenvt <nguyenvt@sheridancollege.ca>
2021-05-03 12:17:40 -0400

    first version

---
+++ src/soduku/SodukuMain.java
@@ -0,0 +1,10 @@
+package soduku;
+
+public class SodukuMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/soduku/SodukuTool.java
@@ -0,0 +1,5 @@
+package soduku;
+
+public class SodukuTool {
+
+}
